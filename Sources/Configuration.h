f712dadde591116beadbb7091aeb39a3fe32b343
Stephen H. Gerstacker <stephen@gerstacker.us>
2020-11-27 17:28:47 -0500

    Restructure before further development

---
+++ Sources/Configuration.h
@@ -0,0 +1,12 @@
+//
+//  Configuration.h
+//  Woodpeckers
+//
+//  Created by Stephen H. Gerstacker on 2020-11-25.
+//  Copyright © 2020 Stephen H. Gerstacker. All rights reserved.
+//
+
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#endif /* CONFIGURATION_H */
---
+++ Tests/DummyTest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(DummyTest, Dummy) {
+    ASSERT_EQ(1, 1);
+}

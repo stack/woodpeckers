4afa5836ed5498be8817df5c9ee6a652758a4309
Stephen H. Gerstacker <stephen@gerstacker.us>
2020-11-29 18:55:50 -0500

    Initial option parsing

--- Sources/main.c
+++ Sources/main.c
@@ -1,23 +1,113 @@
+//
+//  main.c
+//  Woodpeckers
+//
+//  Created by Stephen H. Gerstacker on 2020-11-27.
+//  Copyright © 2020 Stephen H. Gerstacker. All rights reserved.
+//
+
+#include "config.h"
+
+#include <getopt.h>
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "Configuration.h"
-#include "EventLoop.h"
+#include "Macros.h"
+#include "Log.h"
+
+// MARK: - Constants & Globals
+
+#define TAG "Main"
+
+static struct option Options[] = {
+    { "version", no_argument,       NULL, 'v' },
+    { "help",    no_argument,       NULL, 'h' },
+    { "config",  required_argument, NULL, 'c' },
+    { "debug",   no_argument,       NULL, 'd' },
+    { NULL,      0,                 NULL, 0   }
+};
+
+
+// MARK: - Prototypes
+
+static void PrintUsage(void);
+
+
+// MARK: - Main
 
 int main(int argc, char **argv) {
+    // Parse options
+    bool debugMode = false;
+    char *configPath = NULL;
 
-    if (argc != 2) {
+    while (true) {
+        int result = getopt_long(argc, argv, "vhc:d", Options, NULL);
+
+        if (result == -1) {
+            break;
+        }
+
+        switch (result) {
+            case 'h':
+                PrintUsage();
+                return EXIT_SUCCESS;
+                break;
+            case 'c':
+                SAFE_DESTROY(configPath, free);
+                configPath = strdup(optarg);
+                break;
+            case 'd':
+                debugMode = true;
+                break;
+
+        }
+    }
+
+    // Sanity check
+    if (configPath == NULL) {
+        fprintf(stderr, "A config file is required\n");
         return EXIT_FAILURE;
     }
 
-    ConfigurationRef configuration = ConfigurationCreateFromFile(argv[1]);
-
-    if (configuration == NULL) {
-        printf("Failed to load configuration\n");
+    // Set up logging
+    if (debugMode) {
+        LogEnableConsoleOutput(true);
+        LogEnableSystemOutput(false);
     } else {
-        ConfigurationDestroy(configuration);
+        LogEnableConsoleOutput(false);
+        LogEnableSystemOutput(true);
     }
 
+    LogSetUp(LogLevelVerbose);
+
+    LogI(TAG, "Woodpeckers %s", PROJECT_VERSION);
+
+    // Load the configuration file
+    ConfigurationRef configuration = ConfigurationCreateFromFile(configPath);
+
+    if (configuration == NULL) {
+        LogE(TAG, "Failed to load configuration from %s", configPath);
+        return EXIT_FAILURE;
+    }
+
+    LogI(TAG, "Loaded configuration from %s", configPath);
+
+    // Clean up
+    SAFE_DESTROY(configuration, ConfigurationDestroy);
+
     return EXIT_SUCCESS;
+}
+
+
+// MARK: - Utilities
+
+static void PrintUsage() {
+    printf("Usage: Woodpeckers [options]\n");
+    printf("    -v, --version             Print the version number\n");
+    printf("    -h, --help                Print this help message\n");
+    printf("    -c, --config=CONFIG       Path to the required config file\n");
+    printf("    -d, --debug               Run in debug mode\n");
 }
